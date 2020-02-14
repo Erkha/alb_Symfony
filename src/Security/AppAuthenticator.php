7371dc5899dc7be4f79920c46ed42a626b73bde9
erkha <jmtatout@gmail.com>
2020-02-14 10:14:59 +0100

    install + bugs mineurs

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -13,7 +13,7 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
 use Symfony\Component\Security\Core\Security;
-use Symfony\Component\Security\Core\User\User;
+use App\Entity\User;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Csrf\CsrfToken;
